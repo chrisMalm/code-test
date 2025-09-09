f2478d5be2d3623584138de21bc0c38fd3bba4dc
christoffer malmgren <malmgrenchristoffer@hotmail.com>
2025-09-09 16:05:54 +0200

    add  logs for error tracing on vercel

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,22 +1,34 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  connectionString:
-    process.env.DATABASE_URL ||
-    "postgres://postgres:2126@localhost:5432/usersDb",
-  ssl: {
-    rejectUnauthorized: false, // krävs för Neon serverless på Vercel
-  },
-});
+let pool;
 
-pool.connect((err, client, release) => {
-  if (err) {
-    console.error("Databasanslutning misslyckades:", err.stack);
-  } else {
-    console.log("Databas ansluten!");
-    release();
-  }
-});
+if (!global._pool) {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }, // Neon kräver detta
+  });
+  global._pool = pool;
+} else {
+  pool = global._pool;
+}
+
+// const pool = new Pool({
+//   connectionString:
+//     process.env.DATABASE_URL ||
+//     "postgres://postgres:2126@localhost:5432/usersDb",
+//   ssl: {
+//     rejectUnauthorized: false, // krävs för Neon serverless på Vercel
+//   },
+// });
+
+// pool.connect((err, client, release) => {
+//   if (err) {
+//     console.error("Databasanslutning misslyckades:", err.stack);
+//   } else {
+//     console.log("Databas ansluten!");
+//     release();
+//   }
+// });
 // const pool = new Pool({
 //   user: "postgres",
 //   host: "localhost",
